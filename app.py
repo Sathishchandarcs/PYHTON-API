64856259285e0291738edc6731ed4aa59e025ae7
Sathishchandarcs <chandasa@nexturn.com>
2023-09-14 17:53:15 +0000

    solved issue on schema and docker.

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
 
 from db import db
 from blocklist import BLOCKLIST
